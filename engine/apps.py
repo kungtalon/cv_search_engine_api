5f6309c2c4cab7caf286721b6f37358e7585e0c4
kungtalon <dhcshig@163.com>
2021-12-27 23:00:38 -0500

    fix

--- engine/apps.py
+++ engine/apps.py
@@ -15,7 +15,7 @@ class ApiConfig(AppConfig):
     with open(MODEL_FILE, 'rb') as f:
         model = pkl.load(f)
     model.indexes = {}
-    pt.init(version=5.6, helper_version='0.0.6')
+    pt.init()
     for field in FIELDS:
         index_rf = settings.MODEL_DIR / 'index' / field / 'data.properties'
         if not os.path.exists(index_rf):
